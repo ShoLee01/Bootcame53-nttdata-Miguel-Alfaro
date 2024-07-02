2e2ce62bc445343725445c4666f701016b92122a
Miguel Alfaro <miguelalfaro2017@gmail.com>
2024-07-02 11:07:38 -0500

    refactor: rename the project microservice product

--- Product_Microservice/src/main/java/com/nttdata/bank/products/ProductMicroserviceApplication.java
+++ Product_Microservice/src/main/java/com/nttdata/bank/products/ProductMicroserviceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProdcutMicroserviceApplication {
+public class ProductMicroserviceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProdcutMicroserviceApplication.class, args);
+		SpringApplication.run(ProductMicroserviceApplication.class, args);
 	}
 
 }
